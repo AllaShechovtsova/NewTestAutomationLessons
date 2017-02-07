c699b3d3f0c26019baa0854227773bc7f6e259d2
AllaShechovtsova <alusik82@gmail.com>
2017-02-07 15:56:02 +0200

    New task with TextInput

--- NewTestAutomationFramework/src/lesson3/practica/HowManyWordsInSentence.java
+++ NewTestAutomationFramework/src/lesson3/practica/HowManyWordsInSentence.java
@@ -1,5 +1,11 @@
 package lesson3.practica;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HowManyWordsInSentence {
 	public int wordCount (String mySentence, String myWord){
 		int wordQuantity=0;
@@ -12,13 +18,27 @@ public class HowManyWordsInSentence {
 		}
 		return wordQuantity;
 	}
-
-	public static void main(String[] args) {
+	private  void readFile1(String fileName, String myWord) throws IOException {
+		File file = new File(fileName);
+		FileInputStream fis = new FileInputStream(file);
+	 
+		//Construct BufferedReader from InputStreamReader
+		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+	 
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			System.out.println("В предложении "+line+ " слово "+ myWord+ " встречается " +wordCount(line, myWord)+" раз ");	
+		}
+	 
+		br.close();
+	}
+	public static void main(String[] args) throws IOException {
 		String mySentence=" Work is hard but Work is intresting";
 		String myWord= "Work";
 		HowManyWordsInSentence myInstance=new HowManyWordsInSentence();
 		
-		System.out.println("В предложении "+mySentence+ " слово "+ myWord+ " встречается " +myInstance.wordCount(mySentence, myWord)+" раз ");
+	
+		myInstance.readFile1("C:\\Users\\Пользователь\\git\\asproject\\NewTestAutomationFramework\\config\\myTextFile.txt","agreement");
 
 	}
 
